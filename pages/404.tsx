eb20518fb88f202be947f96db77ff9543a5d075f
古瀬良子 <ryoko@furuseryoukonoMacBook-Pro.local>
2022-09-01 14:45:48 +0900

    frontend

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const CustomErrorPage = () => {
+
+  return (
+    <>
+      <h1>404</h1>
+      <p>このページは存在しません</p>
+      {/* <Link href="/">ホームに戻る</Link> */}
+    </>
+  )
+}
+export default CustomErrorPage
